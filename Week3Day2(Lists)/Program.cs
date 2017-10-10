6dda52115866910e4a0ab15e7ecc04da60ab7d80
Shane Moodie <shanepmoodie96@gmail.com>
2017-10-10 13:12:23 -0400

    Initial commit

---
+++ Week3Day2(Lists)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Day2_Lists_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List always gets <> to define data type (int, string, double, ect.)
+         //class|data|nameoflist                    elements in list
+            List<int> testScores = new List<int>() { 98, 100, 76, 84, 93 };
+        }
+    }
+}
